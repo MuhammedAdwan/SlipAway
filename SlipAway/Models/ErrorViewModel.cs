4ac35a3fa2f4765d005232dc83a58b99e51d84d7
MuhammedAdwan <133061822+MuhammedAdwan@users.noreply.github.com>
2023-10-10 14:42:39 -0400

    Add project files.

---
+++ SlipAway/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SlipAway.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
